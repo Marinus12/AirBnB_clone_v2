3d5730bc90228c43bdeac428f8e164c00c7dc02e
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-04-19 11:42:31 -0700

    0x04. AirBnB clone - Web framework

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,55 @@
 #!/usr/bin/env bash
-# Script that sets up web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# Install Nginx if not already installed
+if ! command -v nginx &> /dev/null; then
+    apt-get update
+    apt-get -y install nginx
+fi
 
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+# Create necessary directories if they don't exist
+mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-# Remove existing symbolic link if it exists
-sudo rm -rf /data/web_static/current
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
-# Create a new symbolic link
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+# Create or recreate symbolic link
+if [ -L /data/web_static/current ]; then
+    rm /data/web_static/current
+fi
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+# Give ownership of the /data/ folder to the ubuntu user and group recursively
+chown -R ubuntu:ubuntu /data/
 
-# Add location configuration to Nginx
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Update Nginx configuration
+config_file="/etc/nginx/sites-available/default"
+nginx_config="server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}"
+echo "$nginx_config" | tee "$config_file" > /dev/null
 
 # Restart Nginx
-sudo systemctl restart nginx
+sudo service nginx restart
+
+exit 0
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,4 +1,4 @@
-#!/uar/bin/python3
+#!/usr/bin/python3
 """Script that starts a Flask web application."""
 
 from flask import Flask
